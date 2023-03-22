39ccf48e09b18557c7a9bcd20d1032337ee9d3f3
sn103354 <noa.second@etu.unice.fr>
2023-03-22 21:22:11 +0100

    Add index + gestion and stylesheets

---
+++ js/menu.js
@@ -0,0 +1,29 @@
+function menuOnload() {
+    const menu = document.querySelector('#menu');
+    const menuArrowSVG = document.querySelector('#menuArrowSVG');
+    const menuHomeText = document.querySelector('#menuHomeText');
+    const menuTransportText = document.querySelector('#menuTransportText');
+    const menuServicesText = document.querySelector('#menuServicesText');
+    const menuRHText = document.querySelector('#menuRHText');
+    const menuPompesText = document.querySelector('#menuPompesText');
+
+    menuArrowSVG.addEventListener('click', () => {
+        if (menuArrowSVG.classList.contains('menuExtanded')) {
+            menu.classList.remove('menuExtanded');
+            menuArrowSVG.classList.remove('menuExtanded');
+            menuHomeText.classList.remove('menuExtanded');
+            menuTransportText.classList.remove('menuExtanded');
+            menuServicesText.classList.remove('menuExtanded');
+            menuRHText.classList.remove('menuExtanded');
+            menuPompesText.classList.remove('menuExtanded');
+        } else {
+            menu.classList.add('menuExtanded');
+            menuArrowSVG.classList.add('menuExtanded');
+            menuHomeText.classList.add('menuExtanded');
+            menuTransportText.classList.add('menuExtanded');
+            menuServicesText.classList.add('menuExtanded');
+            menuRHText.classList.add('menuExtanded');
+            menuPompesText.classList.add('menuExtanded');
+        }
+    });
+}
