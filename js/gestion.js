c588f7f8a92e4d38a68b1bd8830741f1c6e54a8b
sn103354 <noa.second@etu.unice.fr>
2023-03-26 22:13:21 +0200

    Add searchBar animation in gestion page

---
+++ js/gestion.js
@@ -0,0 +1,19 @@
+function gestionOnload() {
+    const searchBar = document.getElementById("searchBar");
+    const searchIcon = document.getElementById("searchIcon");
+    const searchBtn = document.getElementById("searchBtn");
+
+    searchBar.addEventListener("input", () => {
+        if (searchBar.value === "") {
+            searchIcon.style.width = "0px";
+            searchIcon.style.height = "0px";
+
+            searchBtn.style.marginLeft = "inherit !important"
+        } else {
+            searchIcon.style.width = "35px";
+            searchIcon.style.height = "35px";
+
+            searchBtn.style.marginLeft = "25px !important"
+        }
+    });
+}
